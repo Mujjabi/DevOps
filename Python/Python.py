d541a5b4a8e730a8e27dbdc515afe0888169e84e
Chris_VSCode <mujjabi2@illinois.edu>
2023-09-27 15:14:42 -0500

    Docker exercise and python.py files modified

--- Python/Python.py
+++ Python/Python.py
@@ -1,3 +1,49 @@
-print('im here to learn python')
-Miles = "My Guy"
-print(Miles)
+{
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "data": {"url": "data/movies.json"},
+    "mark": "point",
+    "encoding": {
+        "x": {
+            "field": "US Gross",
+            "type": "quantitative"
+        },
+        "y": {
+            "field": "Worldwide Gross", "type": "quantitative"
+        },
+        "tooltip": {"field": "Title"}
+    }
+}
+
+# transformation
+{
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "data": {"url": "data/movies.json"},
+    "params": [
+        {"name": "myfirstparam", "select": "interval"}],
+    "hconcat": [
+        {"mark": "bar",
+         "encoding": {
+             "x": {"field": "IMDB Rating", "type": "quantitative",
+                   "bin": true},
+             "y": {"aggregate": "mean", "field": "US Gross"}
+         }
+         },
+        {
+            "transform": [
+                {"filter": {"param": "myfirstparam"}}
+            ],
+            "mark": "point",
+            "encoding": {
+                "x": {
+                    "field": "US Gross",
+                    "type": "quantitative"
+                },
+                "y": {
+                    "field": "Worldwide Gross", "type": "quantitative"}
+            }
+        }
+    ]
+}
+
+
+# With different colors
